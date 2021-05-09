2bd31f1fc5a5713197587e35ca13b99930791e8c
csxjf <43996506+csxjf@users.noreply.github.com>
2021-05-09 11:50:41 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,7 +11,7 @@ fi
 
 ##############################  作  者  昵  称  （必填）  ##############################
 # 使用空格隔开
-author_list="monk_ichenzhe monk_normal LongZhuZhu"
+author_list="monk_ichenzhe monk_normal LongZhuZhu jddj"
 
 ##############################  作  者  脚  本  地  址  URL  （必填）  ##############################
 # 例如：https://raw.sevencdn.com/whyour/hundun/master/quanx/jx_nc.js
@@ -20,8 +20,8 @@ author_list="monk_ichenzhe monk_normal LongZhuZhu"
 
 scripts_base_url_1=https://raw.githubusercontent.com/csxjf/AutoSyncScript/monk/i-chenzhe/
 scripts_base_url_2=https://raw.githubusercontent.com/csxjf/AutoSyncScript/monk/normal/
-scripts_base_url_3=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/qx/
-
+scripts_base_url_3=https://raw.githubusercontent.com/csxjf/AutoSyncScript/longzhuzhu/qx/
+scripts_base_url_4=https://raw.githubusercontent.com/csxjf/AutoSyncScript/passerby-b/
 
 ## 添加更多脚本地址URL示例：scripts_base_url_3=https://raw.sevencdn.com/whyour/hundun/master/quanx/
 
@@ -30,6 +30,7 @@ scripts_base_url_3=https://raw.githubusercontent.com/nianyuguai/longzhuzhu/main/
 my_scripts_list_1="z_fanslove.js z_health_community.js z_health_energy.js z_marketLottery.js z_shake.js z_shop_captain.js z_xmf.js z_mother_jump.js z_wish.js"
 my_scripts_list_2="monk_inter_shop_sign.js monk_shop_follow_sku.js monk_shop_lottery.js adolf_oppo.js"
 my_scripts_list_3="jd_super_redrain.js jd_half_redrain.js"
+my_scripts_list_4="jddj_bean.js jddj_cookie.js jddj_fruit.js jddj_fruit_collectWater.js jddj_getPoints.js jddj_plantBeans.js"
 
 ##############################  随  机  函  数  ##############################
 rand() {
