0b81fc6faac53852449c60ddbb9efb5020ca1af4
csxjf <43996506+csxjf@users.noreply.github.com>
2021-05-17 19:54:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -30,7 +30,7 @@ scripts_base_url_6=https://raw.githubusercontent.com/passerby-b/didi_fruit/main/
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
 # 将相应作者的脚本填写到以下变量中
 my_scripts_list_1="z_fanslove.js z_health_community.js z_health_energy.js z_marketLottery.js z_shake.js z_shop_captain.js z_xmf.js z_mother_jump.js z_wish.js"
-my_scripts_list_2="monk_inter_shop_sign.js monk_shop_follow_sku.js monk_shop_lottery.js adolf_oppo.js"
+my_scripts_list_2="monk_inter_shop_sign.js monk_shop_follow_sku.js monk_shop_lottery.js adolf_oppo.js adolf_pk.js"
 my_scripts_list_3="jd_super_redrain.js jd_half_redrain.js"
 my_scripts_list_4="jddj_bean.js jddj_cookie.js jddj_fruit.js jddj_fruit_collectWater.js jddj_getPoints.js jddj_plantBeans.js"
 my_scripts_list_5="adolf_ETIP.js adolf_haier.js monk_shop_add_to_car.js"
